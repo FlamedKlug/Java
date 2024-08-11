c7f86f6cca1951891ca5e38e06145ed76df18c21
FlamedKlug <kluge@bk.ru>
2024-08-11 16:53:37 +0300

    clock realise 3

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int seconds = scan.nextInt();
         seconds = seconds % 86400;
-        System.out.println((seconds / 3600) + ":" +
-                ((((seconds % 3600) / 60) >= 10) ? ((seconds % 3600) / 60) : ("0" + ((seconds % 3600) / 60)))
-                + ":" + ((((seconds % 3600) % 60) >= 10) ? ((seconds % 3600) % 60) : "0" + ((seconds % 3600) % 60)));
+        System.out.printf("%02d:%02d:%02d", seconds / 3600, seconds % 3600 / 60, seconds % 3600 % 60);
     }
 }
